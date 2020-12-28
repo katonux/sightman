329d7cc89cec8924f5cfe1a65ba874a232260c5e
katonux <kato-shota@outlook.jp>
2020-12-28 13:06:48 +0900

    eslint, prettierの設定を編集

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   root: true,
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    perser: 'babael-eslint',
+    sourceType: 'module'
+  },
   env: {
     browser: true,
     node: true,
+    es6: true
   },
   extends: [
+    'eslint:recommended',
     '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:nuxt/recommended',
+    // 'plugin:vue/vue3-recommended',
   ],
-  plugins: ['prettier'],
+  plugins: ['vue'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'semi': ['error', 'never']
+  },
 }
